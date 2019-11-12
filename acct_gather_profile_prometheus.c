c45e6a73872f76c436e61b4f4c3647e49083845c
Quoding <46576765+Quoding@users.noreply.github.com>
2019-11-12 11:08:17 -0500

    Update acct_gather_profile_prometheus.c
    Changed the header

--- acct_gather_profile_prometheus.c
+++ acct_gather_profile_prometheus.c
@@ -2,47 +2,7 @@
  *  acct_gather_profile_Prometheus.c - slurm accounting plugin for Prometheus
  *				     profiling.
  *****************************************************************************
- *  Author: Carlos Fenoy Garcia
- *  Copyright (C) 2016 F. Hoffmann - La Roche
- *
- *  Based on the HDF5 profiling plugin and Elasticsearch job completion plugin.
- *
- *  Portions Copyright (C) 2013 Bull S. A. S.
- *		Bull, Rue Jean Jaures, B.P.68, 78340, Les Clayes-sous-Bois.
- *
- *  Portions Copyright (C) 2013 SchedMD LLC.
- *
- *  This file is part of Slurm, a resource management program.
- *  For details, see <http://www.schedmd.com/slurmdocs/>.
- *  Please also read the included file: DISCLAIMER.
- *
- *  Slurm is free software; you can redistribute it and/or modify it under
- *  the terms of the GNU General Public License as published by the Free
- *  Software Foundation; either version 2 of the License, or (at your option)
- *  any later version.
- *
- *  In addition, as a special exception, the copyright holders give permission
- *  to link the code of portions of this program with the OpenSSL library under
- *  certain conditions as described in each individual source file, and
- *  distribute linked combinations including the two. You must obey the GNU
- *  General Public License in all respects for all of the code used other than
- *  OpenSSL. If you modify file(s) with this exception, you may extend this
- *  exception to your version of the file(s), but you are not obligated to do
- *  so. If you do not wish to do so, delete this exception statement from your
- *  version.  If you delete this exception statement from all source files in
- *  the program, then also delete it here.
- *
- *  Slurm is distributed in the hope that it will be useful, but WITHOUT ANY
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- *  details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with Slurm; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
- *
- *  This file is patterned after jobcomp_linux.c, written by Morris Jette and
- *  Copyright (C) 2002 The Regents of the University of California.
+ * Based on the InfluxDB Plugin.
  \*****************************************************************************/
 
 #include <stdio.h>
